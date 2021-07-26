3b37cb9d1f5977042577d7ab667f0ef886f61956
ELLEE <leeeunlin@icloud.com>
2021-07-26 22:36:45 +0900

    fix : 데이터베이스의 비밀번호 입력란 추가
    주석 추가

--- DatabaseUpdate.py
+++ DatabaseUpdate.py
@@ -1,6 +1,8 @@
 import pymysql
 
-conn = pymysql.connect(host = "127.0.0.1",port = 3306,user = "root",password = "hyena!!")
+passwd = input("데이터베이스의 비밀번호를 입력\n")
+
+conn = pymysql.connect(host = "127.0.0.1",port = 3306,user = "root",password = passwd)
     
 cur = conn.cursor()
 cur.execute("show databases")
@@ -9,11 +11,11 @@ rows = cur.fetchall()
 
 _list = []
 for index in rows:
-    _list.append(index[0])
+    _list.append(index[0]) # tuple값은 수정이 안되니 리스트로 집어넣어주자
 
 dbexe = input("Project DB에 수정할 명령어 입력 \n")
 for row in _list:
-    if 'project' in row:
+    if 'project' in row: # 리스트에 넣은 값 중 project 스트링이 있는것만 골라서 넣자
         cur.execute("use %s;" % row)
         cur.execute(dbexe)
         print(row, "Finish")
